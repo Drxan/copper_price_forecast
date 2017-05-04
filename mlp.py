b4b742cc05ae61f059ddc7823619b32567b73063
李寅威 <251469031@qq.com>
2017-05-04 17:56:40 +0800

    add trend features, min_max_scaler and data visualization

--- mlp.py
+++ mlp.py
@@ -5,12 +5,14 @@
 @Author: liyinwei
 @E-mail: coridc@foxmail.com
 @Time: 2017/4/25 9:08
-@Description: TODO
+@Description: 采用MLP进行铜价预测
 """
 
 import mysql.connector
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPRegressor
 from sklearn.metrics import explained_variance_score, mean_absolute_error, mean_squared_error, median_absolute_error, \
@@ -27,11 +29,33 @@ def read_data():
                                        use_unicode=True)
         sql = "SELECT * FROM (SELECT * FROM shfe_daily WHERE product_id != '总计' AND product_name != '小计' ORDER BY volume_i) AS a WHERE a.product_name = '铜' AND a.price_date > '2015-01-05' GROUP BY a.price_date, a.product_id ORDER BY a.price_date;"
         df = pd.read_sql(sql, conn)
+        tuples = list(zip(*[range(len(df)), df.price_date]))
+        index = pd.MultiIndex.from_tuples(tuples, names=['id', 'date'])
+        df.index = index
         return df
     except Exception as e:
         print(e)
     finally:
         conn.close()
+
+
+def data_visualization(df):
+    """
+    原始数据可视化
+    """
+    x_values = df.price_date
+    y_values = df.close_price_i
+
+    plt.figure(figsize=(10, 6))
+    plt.title('history copper price')
+    plt.xlabel('')
+    plt.ylabel('history price(rmb/t)')
+
+    plt.plot(x_values, y_values, '-', label='history price')
+
+    plt.legend(loc='upper right')
+
+    plt.show()
 
 
 def feature_engineering(df):
@@ -42,8 +66,13 @@ def feature_engineering(df):
     df = pre_process(df).copy()
     # 特征构建
     df = feature_construction(df)
+    df.to_excel('df.xlsx')
     # 特征选择
     df = feature_selection(df)
+    # 删除求历史均值带来的多余的样本(n_sample = max(get_history_days()))
+    df = delete_redundant_samples(df)
+    # 归一化处理
+    df = pp_min_max_scale(df, scaler)
     return df
 
 
@@ -62,13 +91,14 @@ def feature_construction(df):
     """
     特征构建
     """
+    df = fc_trend(df)
     df = fc_past_avg(df)
     return df
 
 
 def feature_selection(df):
     """
-    特征选择 
+    特征选择
     """
     label_column = 'close_price_i'
     features = get_selected_features()
@@ -76,14 +106,65 @@ def feature_selection(df):
     return df[features]
 
 
+def delete_redundant_samples(df):
+    """
+    删除求历史均值带来的多余的样本(n_sample = max(get_history_days()))
+    """
+    return df.iloc[max(get_history_days()):]
+
+
+def pp_min_max_scale(df, scaler):
+    """
+    特征值归一化处理
+    """
+    # 保存index信息及column信息
+    index = df.index
+    columns = df.columns
+    # 对特征进行归一化
+    feature_scaled = scaler.fit_transform(df.iloc[:, :-1])
+
+    target = np.array(df.iloc[:, -1])
+    target.shape = (len(target), 1)
+
+    # 合并归一化后的X和未做归一化的y（归一化后Pandas 的 DataFrame类型会转换成numpy的ndarray类型）
+    df_scaled = pd.DataFrame(np.hstack((feature_scaled, target)))
+    # 重新设置索引及column信息
+    df_scaled.index = index
+    df_scaled.columns = columns
+
+    return df_scaled
+
+
+def pp_weekday(df):
+    """
+    Pre_Process: 将星期中文替换为数值
+    """
+    df['o_weekday'] = df.o_weekday.map({'一': 1, '二': 2, '三': 3, '四': 4, '五': 5, '六': 6, '日': 7}).astype(int)
+    return df
+
+
+def pp_settlement_price_m(df):
+    """
+    Pre_Process: 过滤settlement_price_m字段为空的记录
+    """
+    return df.dropna()
+
+
+def fc_trend(df):
+    """
+    构建历史趋势特征
+    """
+    history_days = get_history_days()
+    df = get_trend(df, history_days)
+    return df
+
+
 def fc_past_avg(df):
     """
     构建过去n天均值作为特征，一方面在特征中加入历史影响，另一方面避免用当天特征预测当天价格的情况
     """
     history_days = get_history_days()
-    for day in history_days:
-        df = get_history_avg(df, day)
-    # print(df.describe())
+    df = get_history_avg(df, history_days)
     return df
 
 
@@ -91,7 +172,8 @@ def get_history_days():
     """
     获取历史数据统计的时间跨度（天/交易日）
     """
-    return [3, 5, 15, 30]
+    # return [1, 3, 5, 15]
+    return [1, 3]
 
 
 def get_history_features():
@@ -118,20 +200,35 @@ def get_common_features():
     """
     原始数据中需要参与预测但不参与构建历史统计特征的特征集合
     """
-    features = ['delivery_month', 'o_year', 'o_month', 'o_day', 'o_weekday', 'o_year_num', 'o_total_num', 'o_trade_day']
+    features = ['delivery_month', 'o_year', 'o_month', 'o_day', 'o_weekday', 'o_year_num', 'o_total_num']
     return features
 
 
-def get_history_avg(df, day):
+def get_trend(df, history_days):
+    """
+    构建历史趋势信息
+    """
+    updown_features = ['zd1_chg']
+    for index, row in df.iterrows():
+        for day in history_days:
+            if index[0] < day:
+                continue
+            for feature in updown_features:
+                df.set_value(index[0], feature + '_trend_' + str(day), df[index[0] - day: index[0]][feature].sum())
+    return df
+
+
+def get_history_avg(df, history_days):
     """
     构建包含历史统计信息特征
     """
     features = get_history_features()
     for index, row in df.iterrows():
-        if index < day:
-            continue
-        for feature in features:
-            df.set_value(index, feature + '_' + str(day), df[index - day: index][feature].mean())
+        for day in history_days:
+            if index[0] < day:
+                continue
+            for feature in features:
+                df.set_value(index[0], feature + '_' + str(day), df[index[0] - day: index[0]][feature].mean())
     return df
 
 
@@ -147,27 +244,14 @@ def get_selected_features():
     return features
 
 
-def pp_weekday(df):
-    """
-    Pre_Process: 将星期中文替换为数值
-    """
-    df['o_weekday'] = df.o_weekday.map({'一': 1, '二': 2, '三': 3, '四': 4, '五': 5, '六': 6, '日': 7}).astype(int)
-    return df
-
-
-def pp_settlement_price_m(df):
-    """
-    Pre_Process: 过滤settlement_price_m字段为空的记录
-    """
-    return df.dropna()
-
-
-def model_evaluate(actual, predict):
+def model_evaluate(actual, predict, train_index, test_index):
     print("explained_variance_score: " + str(explained_variance_score(actual, predict)))
     print("mean_absolute_error: " + str(mean_absolute_error(actual, predict)))
     print("mean_squared_error: " + str(mean_squared_error(actual, predict)))
     print("median_absolute_error: " + str(median_absolute_error(actual, predict)))
     print("r2_score: " + str(r2_score(actual, predict)))
+    # diff = list(map(lambda x: x[0] - x[1], zip(actual, predict)))
+    # print("trend_error_rate" + str(len(list(filter(lambda x: x > 0, diff)))))
 
 
 def model_visualization(actual, predict):
@@ -178,7 +262,7 @@ def model_visualization(actual, predict):
 
     plt.figure(figsize=(10, 6))
     plt.title('copper price forecast model evaluating')
-    plt.xlabel('days')
+    plt.xlabel('samples')
     plt.ylabel('actual price vs. predict price')
     plt.grid(x)
 
@@ -193,23 +277,28 @@ def model_visualization(actual, predict):
 if __name__ == '__main__':
     # 读取原始数据
     raw_data = read_data()
+    # 原始数据可视化
+    data_visualization(raw_data)
+    # 用于归一化的对象
+    scaler = preprocessing.MinMaxScaler()
     # 特征工程
     fed_data = feature_engineering(raw_data)
-    # print(fed_data.describe())
-    X = fed_data[30:][get_selected_features()]
-    y = fed_data[30:]['close_price_i']
+    # features
+    X = fed_data.take(list(range(fed_data.shape[1] - 1)), axis=1)
+    # target
+    y = np.ravel(fed_data.take([fed_data.shape[1] - 1], axis=1))
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
     print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
 
-    pd.DataFrame(y_test).to_excel('y_test.xlsx')
     # 定义一个BP神经网络
     reg = MLPRegressor(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(15,), random_state=1)
     # 训练
     reg.fit(X_train, y_train)
     # 预测
     y_pred = reg.predict(X_test)
+    # 将结果写入文件
     pd.DataFrame(y_pred).to_excel('y_pred.xlsx')
     # 模型评估
-    model_evaluate(y_test, y_pred)
+    model_evaluate(y_test, y_pred, X_train.index, X_test.index)
     # 可视化
     model_visualization(y_test, y_pred)
